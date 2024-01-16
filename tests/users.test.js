dbf633690983fd937642af9dc79db253d114e56e
sibabalwe47 <sibabalwe47@gmail.com>
2024-01-16 12:39:48 +0200

    Added emojis suite :)

--- tests/users.test.js
+++ tests/users.test.js
@@ -1,6 +1,6 @@
 import { should } from 'chai'
-describe('👋 User test suite:', () => {
-    it('🧪 Should have a max of 2 ', (done) => {
+describe('🧪 User tests:', () => {
+    it('Should have a max of 2 ', (done) => {
         const maxNumberOfUsers = 2
         should().equal(maxNumberOfUsers, 2);
         done()
