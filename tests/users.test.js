0f544a86dbe6b1d7c93f6c62978ad55282e92aaf
sibabalwe47 <sibabalwe47@gmail.com>
2024-01-16 12:36:11 +0200

    Modified testing suite

--- tests/users.test.js
+++ tests/users.test.js
@@ -1,11 +1,8 @@
 import { should } from 'chai'
-describe('User test suite:', () => {
-    // Get all usres from API
-    describe('Number of users', () => { 
-        it('should return status 200', (done) => {
-            const maxNumberOfUsers = 2
-            should().equal(maxNumberOfUsers, 2);
-            done()
-        })
+describe('👋 User test suite:', () => {
+    it('🧪 Should have a max of 2 ', (done) => {
+        const maxNumberOfUsers = 2
+        should().equal(maxNumberOfUsers, 2);
+        done()
     })
  })
